3b76244b7615a9148854fcc8636dbee5bab400be
Shema-8 <e.shema2@alustudent.com>
2025-07-04 23:44:38 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
-""" Get the titles of the first 10 hot posts for a given subreddit."""
+"""Get the titles of the first 10 hot posts for a given subreddit using Reddit's JSON API."""
+
 import requests
 
 
 def top_ten(subreddit):
-
+    """Prints the titles of the first 10 hot posts in a given subreddit."""
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://www.reddit.com/r/{}.json".format(subreddit) 
+    subreddit_url = f"https://reddit.com/r/{subreddit}/hot.json?limit=10"
     response = requests.get(subreddit_url, headers=headers)
 
     if response.status_code == 200:
         json_data = response.json()
-        for i in range(10):
-            print(
-                json_data.get('data')
-                .get('children')[i]
-                .get('data')
-                .get('title')
-            )
+        posts = json_data.get('data', {}).get('children', [])
+        for post in posts[:10]:
+            print(post.get('data', {}).get('title'))
     else:
         print(None)
        
